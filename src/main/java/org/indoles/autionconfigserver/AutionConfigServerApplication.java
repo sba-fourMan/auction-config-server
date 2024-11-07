4d9b7e4f7d0a199bb4d886a380689a510ee96c94
jay-so <merry9504@naver.com>
2024-11-07 13:42:23 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/org/indoles/autionconfigserver/AutionConfigServerApplication.java
@@ -0,0 +1,13 @@
+package org.indoles.autionconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutionConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutionConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/indoles/autionconfigserver/AutionConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.indoles.autionconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutionConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
