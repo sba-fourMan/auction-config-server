5077cea4de6d86ff98779a31e14ceb552dd44010
jay-so <merry9504@naver.com>
2024-11-07 15:03:21 +0900

    sytle: 프로젝트명 오타 수정

--- src/main/java/org/indoles/autionconfigserver/AuctionConfigServerApplication.java
+++ src/main/java/org/indoles/autionconfigserver/AuctionConfigServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AutionConfigServerApplication {
+public class AuctionConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AutionConfigServerApplication.class, args);
+        SpringApplication.run(AuctionConfigServerApplication.class, args);
     }
 
 }
